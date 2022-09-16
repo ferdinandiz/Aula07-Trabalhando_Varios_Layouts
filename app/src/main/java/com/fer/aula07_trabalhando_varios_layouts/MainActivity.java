0deeb75b73196419c2e85a82320d991d48f19361
ferdinandiz <80607754+ferdinandiz@users.noreply.github.com>
2022-09-16 20:09:32 -0300

    Aula sobre utilização de Scroll View.

---
+++ app/src/main/java/com/fer/aula07_trabalhando_varios_layouts/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fer.aula07_trabalhando_varios_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
